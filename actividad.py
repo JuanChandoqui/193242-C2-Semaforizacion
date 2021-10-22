d83cbb4a39b8e83386e9b323e4a38e8a4a727882
JuanChandoqui <chandoquimorales@gmail.com>
2021-10-22 01:48:25 -0500

    activity working

--- actividad.py
+++ actividad.py
@@ -6,22 +6,22 @@ class TenedorFilosofo(threading.Thread):
         threading.Thread.__init__(self)
         self.tenedores = tenedores
         self.filosofosNum = filosofosNum
-        self.datoTemporal =  self.filosofosNum + 1 % 5
+        self.datoTemporal =  self.filosofosNum - 1
    
     def hilosFilosofos(self):
         while True:
             print("Filosofo iniciando", self.filosofosNum)
             time.sleep(2)
-            print("Filosofo ", self.filosofosNum, "pasando tenedor del lado izquierdo")
+            print("Filosofo :", self.filosofosNum, "pasando tenedor del lado izquierdo")
             self.tenedores[self.filosofosNum].acquire()
             time.sleep(2)
-            print("Filosofo ", self.filosofosNum, "recoge tenedor del lado derecho")
+            print("Filosofo :", self.filosofosNum, "recoge tenedor del lado derecho")
             self.tenedores[self.datoTemporal].acquire()
             time.sleep(2)
-            print("Filosofo ", self.filosofosNum, "libre derecho")
+            print("Filosofo :", self.filosofosNum, "libre derecho")
             self.tenedores[self.datoTemporal].release()
             time.sleep(2)
-            print("Filosofo ", self.filosofosNum, "libre izquierdo")
+            print("Filosofo :", self.filosofosNum, "libre izquierdo")
             self.tenedores[self.filosofosNum].release()
             time.sleep(2)
    
@@ -39,3 +39,4 @@ for i in range(0,5):
     print("for dos: ", i)
     total = TenedorFilosofo(tenedorArray, i)
     total.start()
+    time.sleep(1)
